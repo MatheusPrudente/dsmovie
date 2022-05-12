18e22a6e3aefaba80e736ca27ea7081bb0dc01f7
Matheus Prudente <maths.henrique.14.2001@gmail.com>
2022-05-12 01:26:16 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,31 @@
+import MovieCards from "components/MovieCards";
 import Pagination from "components/Pagination";
 
-function Listing(){
-    return(
-        <Pagination />
-
-    );
+function Listing() {
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+            <div className="col-sm-6 col-lg-4 col-xl-3">
+                <MovieCards />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3">
+                <MovieCards />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3">
+                <MovieCards />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3">
+                <MovieCards />
+            </div>
+            <div className="col-sm-6 col-lg-4 col-xl-3">
+                <MovieCards />
+            </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
